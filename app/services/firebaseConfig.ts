4c24049e6e19261fc25479b453ec154683542feb
Umid <kumid@inbox.ru>
2024-12-18 14:18:17 +0300

    fixate

--- app/services/firebaseConfig.ts
+++ app/services/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 import Constants from "expo-constants";
 
 const firebaseConfig = {
@@ -15,5 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { db, auth };
